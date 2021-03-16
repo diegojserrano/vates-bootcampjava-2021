5bffd71f07cdabd398ab3297cab52ec654da0f51
Diego Serrano <diegojserrano@gmail.com>
2021-03-16 19:35:21 -0300

    ejercicios clase 6

--- clase06/empleados/src/Programa.java
+++ clase06/empleados/src/Programa.java
@@ -1,10 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Programa {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         ListaEmpleados lista = new ListaEmpleados();
+        Scanner archivo = new Scanner(new File("empleados.txt"));
 
-        lista.agregar(new Obrero(12,"Juan",50000,25));
-        lista.agregar(new Administrativo(34, "Mario",50000,true));
+        archivo.useDelimiter("[;\\n]");
+        while(archivo.hasNextInt()) {
+            Empleado nuevo = null;
+            int tipo = archivo.nextInt();
+            int legajo = archivo.nextInt();
+            String nombre = archivo.next();
+            float sueldo = archivo.nextFloat();
+            switch (tipo) {
+                case 1: // es un obrero
+                    int dias = archivo.nextInt();
+                    nuevo = new Obrero(legajo, nombre, sueldo, dias);
+                    break;
+                case 2:
+                    boolean presentismo = archivo.nextBoolean();
+                    nuevo = new Administrativo(legajo, nombre, sueldo, presentismo);
+                    break;
+                case 3:
+                    float ventas = archivo.nextFloat();
+                    nuevo = new Vendedor(legajo, nombre, sueldo, ventas);
+                    break;
+            }
 
-        System.out.println(lista.totalSueldos());
+            lista.agregar(nuevo);
+        }
+
+
+        System.out.printf("%15.2f",lista.totalSueldos());
     }
 }
