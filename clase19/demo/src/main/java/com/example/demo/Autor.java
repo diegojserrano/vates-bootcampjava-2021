143b7c847b3a91c71719a58897a0870c23cb8a3a
Diego Serrano <diegojserrano@gmail.com>
2021-05-04 18:59:49 -0300

    Ejercicio clase 19

---
+++ clase19/demo/src/main/java/com/example/demo/Autor.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Autores")
+public class Autor {
+
+    @Id
+    private int id;
+
+    @Column
+    private String nombre;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public Autor() {
+    }
+
+    public Autor(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Autor{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
+}
